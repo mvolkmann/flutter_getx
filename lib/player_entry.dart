add31d8fe34724528909f4f8bd17ecc9aa8aeba3
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-01-12 19:52:51 -0600

    added a comment

--- lib/player_entry.dart
+++ lib/player_entry.dart
@@ -10,6 +10,8 @@ class PlayerEntry extends StatefulWidget {
 }
 
 class _PlayerEntryState extends State<PlayerEntry> {
+  // How does Get.find get the correct controller?
+  // Is it based on the type of the left-hand side?
   final PlayerController playerCtrl = Get.find();
 
   final tec = TextEditingController();
